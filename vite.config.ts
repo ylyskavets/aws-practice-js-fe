274599d3e53aabe16566fbf0bac3f9644fb233fb
Yauhen_Lyskavets <yauhen_lyskavets@epam.com>
2022-10-03 14:09:11 +0200

    Task1
    - Vite config adaptation for Docker

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,   // needed for the DC port mapping to work
+    strictPort: true,
+    port: 3000
+  },
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
